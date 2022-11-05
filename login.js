f179bbe4a1b838495d864ee3686a58431e1f3a05
shjk <shinjk0531@gmail.com>
2022-11-05 11:57:57 +0900

    add login event

---
+++ login.js
@@ -0,0 +1,9 @@
+const id = document.getElementById("id")
+const password = document.getElementById("password")
+const login_enter = document.getElementById("logtin")
+
+function login(event) {
+    
+}
+
+login_enter.addEventListener('click', login)
