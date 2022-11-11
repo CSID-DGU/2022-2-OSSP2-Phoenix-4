1d9fcd3404d6f5f2c229ed68612d60ae26c4ced3
0230cp <rhwocks69@gmail.com>
2022-11-11 14:21:34 +0900

    수정
    수정

--- login/login.js
+++
@@ -1,9 +0,0 @@
-const id = document.getElementById("id")
-const password = document.getElementById("password")
-const login_enter = document.getElementById("login")
-
-function login() {
-    
-}
-
-login_enter.addEventListener('click', login)
