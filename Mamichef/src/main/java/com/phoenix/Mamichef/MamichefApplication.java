e1b766935a034575145a7b41c6d9b8bf6cffdf7d
0230cp <rhwocks69@gmail.com>
2022-11-11 13:54:05 +0900

    spring 기본설정
    spring 기본 설정

---
+++ Mamichef/src/main/java/com/phoenix/Mamichef/MamichefApplication.java
@@ -0,0 +1,13 @@
+package com.phoenix.Mamichef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MamichefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MamichefApplication.class, args);
+	}
+
+}
---
+++ Mamichef/src/test/java/com/phoenix/Mamichef/MamichefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phoenix.Mamichef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MamichefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
