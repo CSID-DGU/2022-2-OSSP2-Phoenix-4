b86f2e25cfd2e4101f5d5749d9f6f6ccd1f97d2f
yonggwonk <gwonluck@dgu.ac.kr>
2022-11-17 23:21:27 +0900

    기본 기능 컨트롤러 추가

---
+++ Mymichef/src/main/java/phoenix/Mymichef/controller/FoodController.java
@@ -0,0 +1,10 @@
+package phoenix.Mymichef.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class FoodController {
+}
+//보류
