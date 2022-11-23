830264a4ac7dd7052d21c129bab092781987299f
shjk0531 <shinjk0531@gmail.com>
2022-11-23 18:48:06 +0900

    update

--- Mymichef/src/main/resources/static/front/js/login.js
+++ Mymichef/src/main/resources/static/front/js/login.js
@@ -1,3 +1,18 @@
+// 로그인 input
+const id = document.getElementById("id");
+const password = document.getElementById("password");
+
+// 아이디 찾기 input
+const findIdName = document.getElementById("find_id_name");
+const findIdEmail = document.getElementById("find_id_email");
+
+// 비밀번호 찾기 input
+const findPwName = document.getElementById("find_pw_name");
+const findPwId = document.getElementById("find_pw_id");
+const findPwEmail = document.getElementById("find_pw_email");
+
+// 회원가입 input
+
 function setFormMessage(formElement, type, message) {
   const messageElement = formElement.querySelector(".form__message");
 
@@ -26,46 +41,42 @@ function clearInputError(inputElement) {
 document.addEventListener("DOMContentLoaded", () => {
   const loginForm = document.querySelector("#login");
   const findIDForm = document.querySelector("#findID");
-  document
-        .querySelector("#linkFindID")
-        .addEventListener("click", (e) => {
-          e.preventDefault();
-          loginForm.classList.add("form--hidden");
-          findIDForm.classList.remove("form--hidden");
-        });
+  document.querySelector("#linkFindID").addEventListener("click", (e) => {
+    e.preventDefault();
+    loginForm.classList.add("form--hidden");
+    findIDForm.classList.remove("form--hidden");
+  });
   document.querySelector("#id_linkLogin").addEventListener("click", (e) => {
-      e.preventDefault();
-      loginForm.classList.remove("form--hidden");
-      findIDForm.classList.add("form--hidden");
-    });
+    e.preventDefault();
+    loginForm.classList.remove("form--hidden");
+    findIDForm.classList.add("form--hidden");
+  });
 
   const findPasswordForm = document.querySelector("#findPassword");
-  document
-      .querySelector("#linkFindPassword")
-      .addEventListener("click", (e) => {
-        e.preventDefault();
-        loginForm.classList.add("form--hidden");
-        findPasswordForm.classList.remove("form--hidden");
-      });
+  document.querySelector("#linkFindPassword").addEventListener("click", (e) => {
+    e.preventDefault();
+    loginForm.classList.add("form--hidden");
+    findPasswordForm.classList.remove("form--hidden");
+  });
   document.querySelector("#pw_linkLogin").addEventListener("click", (e) => {
-      e.preventDefault();
-      loginForm.classList.remove("form--hidden");
-      findPasswordForm.classList.add("form--hidden");
-    });
+    e.preventDefault();
+    loginForm.classList.remove("form--hidden");
+    findPasswordForm.classList.add("form--hidden");
+  });
 
   const createAccountForm = document.querySelector("#createAccount");
   document
-      .querySelector("#linkCreateAccount")
-      .addEventListener("click", (e) => {
-        e.preventDefault();
-        loginForm.classList.add("form--hidden");
-        createAccountForm.classList.remove("form--hidden");
-      });
-  document.querySelector("#linkLogin").addEventListener("click", (e) => {
+    .querySelector("#linkCreateAccount")
+    .addEventListener("click", (e) => {
       e.preventDefault();
-      loginForm.classList.remove("form--hidden");
-      createAccountForm.classList.add("form--hidden");
+      loginForm.classList.add("form--hidden");
+      createAccountForm.classList.remove("form--hidden");
     });
+  document.querySelector("#linkLogin").addEventListener("click", (e) => {
+    e.preventDefault();
+    loginForm.classList.remove("form--hidden");
+    createAccountForm.classList.add("form--hidden");
+  });
 
   loginForm.addEventListener("submit", (e) => {
     e.preventDefault();
@@ -95,6 +106,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+// 회원가입 시 json 형식 데이터 생성
 function joinMembership() {
   const userList = [];
   const userData = {};
@@ -102,6 +114,26 @@ function joinMembership() {
   userData.name = document.getElementById("signupUsername");
   userData.email = document.getElementById("signupUserEmail");
   userData.password = document.getElementById("password");
-
-  userList.push(userData);
+  if (userData.name !== "") {
+    if (userData.email !== "") {
+      if (userData.password !== "") {
+        userList.push(userData);
+        console.log("userdata 생성 완료");
+      } else {
+        console.log("password error");
+      }
+    } else {
+      console.log();
+    }
+  }
 }
+
+function onSubmitClick() {
+  const userID = document.getElementById("username");
+  const userPassword = document.getElementById("password");
+
+  console.log(userID.value, userPassword.value);
+}
+
+const submitBtn = document.getElementById("submitBtn");
+submitBtn.addEventListener("click", onSubmitClick);
