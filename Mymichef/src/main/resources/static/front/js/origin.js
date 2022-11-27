ee55949a960e48d533f04b1ca95cd24be9850372
DESKTOP-BJ8G1LU\nikph <2020113392@dgu.ac.kr>
2022-11-27 10:02:47 +0900

    origin.html, origin.js 가 원래 login.html, login.js입니다들ㅠ

---
+++ Mymichef/src/main/resources/static/front/js/origin.js
@@ -0,0 +1,107 @@
+function setFormMessage(formElement, type, message) {
+  const messageElement = formElement.querySelector(".form__message");
+
+  messageElement.textContent = message;
+  messageElement.classList.remove(
+    "form__message--success",
+    "form__message--error"
+  );
+  messageElement.classList.add(`form__message--${type}`);
+}
+
+function setInputError(inputElement, message) {
+  inputElement.classList.add("form__input--error");
+  inputElement.parentElement.querySelector(
+    ".form__input-error-message"
+  ).textContent = message;
+}
+
+function clearInputError(inputElement) {
+  inputElement.classList.remove("form__input--error");
+  inputElement.parentElement.querySelector(
+    ".form__input-error-message"
+  ).textContent = "";
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const loginForm = document.querySelector("#login");
+  const findIDForm = document.querySelector("#findID");
+  document
+        .querySelector("#linkFindID")
+        .addEventListener("click", (e) => {
+          e.preventDefault();
+          loginForm.classList.add("form--hidden");
+          findIDForm.classList.remove("form--hidden");
+        });
+  document.querySelector("#id_linkLogin").addEventListener("click", (e) => {
+      e.preventDefault();
+      loginForm.classList.remove("form--hidden");
+      findIDForm.classList.add("form--hidden");
+    });
+
+  const findPasswordForm = document.querySelector("#findPassword");
+  document
+      .querySelector("#linkFindPassword")
+      .addEventListener("click", (e) => {
+        e.preventDefault();
+        loginForm.classList.add("form--hidden");
+        findPasswordForm.classList.remove("form--hidden");
+      });
+  document.querySelector("#pw_linkLogin").addEventListener("click", (e) => {
+      e.preventDefault();
+      loginForm.classList.remove("form--hidden");
+      findPasswordForm.classList.add("form--hidden");
+    });
+
+  const createAccountForm = document.querySelector("#createAccount");
+  document
+      .querySelector("#linkCreateAccount")
+      .addEventListener("click", (e) => {
+        e.preventDefault();
+        loginForm.classList.add("form--hidden");
+        createAccountForm.classList.remove("form--hidden");
+      });
+  document.querySelector("#linkLogin").addEventListener("click", (e) => {
+      e.preventDefault();
+      loginForm.classList.remove("form--hidden");
+      createAccountForm.classList.add("form--hidden");
+    });
+
+  loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    // Perform your AJAX/Fetch login
+
+    setFormMessage(loginForm, "error", "Invalid username/password combination");
+  });
+
+  document.querySelectorAll(".form__input").forEach((inputElement) => {
+    inputElement.addEventListener("blur", (e) => {
+      if (
+        e.target.id === "signupUsername" &&
+        e.target.value.length > 0 &&
+        e.target.value.length < 6
+      ) {
+        setInputError(
+          inputElement,
+          "Username must be at least 6 characters in length"
+        );
+      }
+    });
+
+    inputElement.addEventListener("input", (e) => {
+      clearInputError(inputElement);
+    });
+  });
+});
+
+function joinMembership() {
+  const userList = [];
+  const userData = {};
+
+  userData.name = document.getElementById("signupUsername");
+  userData.email = document.getElementById("signupUserEmail");
+  userData.password = document.getElementById("password");
+
+  userList.push(userData);
+}
