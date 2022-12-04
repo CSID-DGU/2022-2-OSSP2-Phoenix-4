8c1fe9c5211b4a7ffa97a5253796276b17d20d90
shjk0531 <shinjk0531@gmail.com>
2022-12-04 15:02:09 +0900

    update

--- Mymichef/src/main/resources/static/front/js/food_input.js
+++ Mymichef/src/main/resources/static/front/js/food_input.js
@@ -1,37 +1,97 @@
 // 서버 음식 api accept
-let foodsData = {};
+let ingredientsData = {};
 $.ajax({
   url: "##",
   type: "POST",
   contentType: "application/json",
-  data: JSON.stringify(foodsData),
+  data: JSON.stringify(ingredientsData),
 
   success: function (response) {
-    console.log("foods api post success");
+    console.log("ingredients api post success");
     console.log(response);
   },
   error: function (error) {
-    console.log("foods api post error");
+    console.log("ingredients api post error");
     console.log(error);
   },
 });
 
-function foodInputInit() {
-  const foodsname = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
+$(document).ready(function () {
+  ingredientInputInit();
+});
+
+function ingredientInputInit() {
+  // 식재료 찾기
+  const ingredientSearch = document.getElementById("ingredientSearch");
+  const ingredientsResult = document.getElementById("ingredientsResult");
+
+  let ingredientsName = [
+    { name: "쌀" },
+    { name: "감자" },
+    { name: "고구마" },
+    { name: "당근" },
+    { name: "콩나물" },
+    { name: "토마토" },
+    { name: "배추" },
+    { name: "오이" },
+    { name: "호박" },
+    { name: "양파" },
+    { name: "마늘" },
+    { name: "돼지고기" },
+    { name: "쇠고기" },
+    { name: "닭고기" },
   ];
+  ingredientsName = ingredientsName.sort((a, b) => {
+    if (a.name > b.name) return 1;
+    if (a.name < b.name) return -1;
+  });
+
+  // 검색창 재료 목록
+  for (let i = 0; i < ingredientsName.length; i++) {
+    const ingredientName = ingredientsName[i].name;
+    const ingredientImg = "../jpg/logo!.png";
+
+    const divName = `<div class="ingredientName">${ingredientName}</div>`;
+    const divImg = `<img class="ingredientImg" src=${ingredientImg} alt=${ingredientName}>`;
+
+    ingredientsResult.innerHTML =
+      ingredientsResult.innerHTML +
+      `<div class="ingredient">` +
+      divImg +
+      divName +
+      `</div>`;
+  }
+
+  // 재료 검색
+  function onSearch(event) {
+    const search = event.target.value.toLowerCase();
+    const items = document.getElementsByClassName("ingredient");
+
+    for (let i = 0; i < items.length; i++) {
+      const item = items[i].getElementsByClassName("ingredientName");
+      if (item[0].innerHTML.toLowerCase().indexOf(search) !== -1) {
+        items[i].style.display = "inline-block";
+      } else {
+        items[i].style.display = "none";
+      }
+    }
+  }
+  ingredientSearch.addEventListener("keyup", onSearch);
+
+  // 재료 정보 입력
+  const infoForm = document.getElementById("ingredientInfoForm");
+  const infoName = document.getElementById("ingredientName");
+  const items = Array.from(document.getElementsByClassName("ingredient"));
+
+  // 재로 클릭 이벤트
+  function onResultClick(event) {
+    const id = event.target.id;
+    console.log(event);
+    console.log(id);
+  }
+  items.forEach((item) => item.addEventListener("click", onResultClick));
+
+  // 사용자 재료 리스트
 }
 
 (function () {
