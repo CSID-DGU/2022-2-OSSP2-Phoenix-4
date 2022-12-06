27887d0fc5a61296390af139ee88869ae58908bf
DESKTOP-BJ8G1LU\nikph <2020113392@dgu.ac.kr>
2022-12-07 03:42:22 +0900

    못하겠어,,,중근오빠에게 토스할게,,,미안해여러분들 찡긋...

--- Mymichef/src/main/resources/static/front/js/mypage.js
+++ Mymichef/src/main/resources/static/front/js/mypage.js
@@ -1,6 +1,11 @@
 const data1 = [];
 const data2 = [];
 
+document.addEventListener("DOMContentLoaded",()=>{
+    const parentForm=document.querySelector("#parent");
+/*와ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ 못해먹겠다 js 후하후하,,,,,,,,,,,,SOS...*/
+
+
 $.ajax({
   url: "/mypage/userInfo",
   type: "POST",
@@ -17,22 +22,51 @@ $.ajax({
     console.log(error.responseText);
   },
 });
-$.ajax({
-  url: "/updateUserInfo",
-  type: "POST",
-  contentType: "application/json",
-  data: JSON.stringify(data2),
 
-  success: function (response) {
-    console.log("updateUserInfo success");
-    console.log(response);
-  },
-  error: function (error) {
-    console.log("updateUserInfo error");
-    console.log(error);
-    console.log(error.responseText);
-  },
-});
+
+
+/*와ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ 못해먹겠다 js 후하후하,,,,,,,,,,,,SOS...*/
+parentForm.addEventListener("submit",(event) => {
+    event.preventDefault();
+
+    const inputName=event.target[0].value;
+    const
+
+    inputEmail = event.target[1].value;
+
+    inputPassword = event.target[2].value;
+
+    inputPhoneNumber=event.target[4].value;
+
+    inputHeight=event.target[5].value;
+
+    inputWeight=event.target[6].value;
+
+    inputAllergy=event.target[7].value;
+
+    inputGender=event.target[8].value;
+/*와ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ 못해먹겠다 js 후하후하,,,,,,,,,,,,SOS...*/
+
+
+
+    $.ajax({
+      url: "/updateUserInfo",
+      type: "POST",
+      contentType: "application/json",
+      data: JSON.stringify(data2),
+
+      success: function (response) {
+        console.log("updateUserInfo success");
+        console.log(response);
+      },
+      error: function (error) {
+        console.log("updateUserInfo error");
+        console.log(error);
+        console.log(error.responseText);
+      },
+    });
+}
+);
 
 (function () {
   "use strict";
@@ -72,5 +106,6 @@ $('.button--trigger').on('click', function(e) {
   $('.button--trigger').hide()
   $('.button--disappear').show();
   $('.button--disappear').removeClass('out').addClass('active');
+});
 });
 
