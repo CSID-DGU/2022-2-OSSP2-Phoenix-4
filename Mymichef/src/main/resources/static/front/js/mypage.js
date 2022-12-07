f98d234d3bca2b22539831bcc2ad4dd025d33ee6
shjk0531 <shinjk0531@gmail.com>
2022-12-07 09:54:10 +0900

    updata

--- Mymichef/src/main/resources/static/front/js/mypage.js
+++ Mymichef/src/main/resources/static/front/js/mypage.js
@@ -1,16 +1,11 @@
-const data1 = [];
-const data2 = [];
-
-document.addEventListener("DOMContentLoaded",()=>{
-    const parentForm=document.querySelector("#parent");
-/*와ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ 못해먹겠다 js 후하후하,,,,,,,,,,,,SOS...*/
-
+let userData = [];
+let data2 = [];
 
 $.ajax({
   url: "/mypage/userInfo",
   type: "POST",
-  contentType: "application/json",
-  data: JSON.stringify(data1),
+  contentType: "json",
+  data: JSON.stringify(userData),
 
   success: function (response) {
     console.log("userInfo success");
@@ -23,50 +18,44 @@ $.ajax({
   },
 });
 
+const parentForm = document.querySelector(".container");
 
+parentForm.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-/*와ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ 못해먹겠다 js 후하후하,,,,,,,,,,,,SOS...*/
-parentForm.addEventListener("submit",(event) => {
-    event.preventDefault();
+  const inputName = event.target[0].value;
+  const inputEmail = event.target[1].value;
 
-    const inputName=event.target[0].value;
-    const
+  inputPassword = event.target[2].value;
 
-    inputEmail = event.target[1].value;
+  inputPhoneNumber = event.target[4].value;
 
-    inputPassword = event.target[2].value;
+  inputHeight = event.target[5].value;
 
-    inputPhoneNumber=event.target[4].value;
+  inputWeight = event.target[6].value;
 
-    inputHeight=event.target[5].value;
+  inputAllergy = event.target[7].value;
 
-    inputWeight=event.target[6].value;
+  inputGender = event.target[8].value;
+  /*와ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ 못해먹겠다 js 후하후하,,,,,,,,,,,,SOS...*/
 
-    inputAllergy=event.target[7].value;
+  $.ajax({
+    url: "/updateUserInfo",
+    type: "POST",
+    contentType: "application/json",
+    data: JSON.stringify(data2),
 
-    inputGender=event.target[8].value;
-/*와ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ 못해먹겠다 js 후하후하,,,,,,,,,,,,SOS...*/
-
-
-
-    $.ajax({
-      url: "/updateUserInfo",
-      type: "POST",
-      contentType: "application/json",
-      data: JSON.stringify(data2),
-
-      success: function (response) {
-        console.log("updateUserInfo success");
-        console.log(response);
-      },
-      error: function (error) {
-        console.log("updateUserInfo error");
-        console.log(error);
-        console.log(error.responseText);
-      },
-    });
-}
-);
+    success: function (response) {
+      console.log("updateUserInfo success");
+      console.log(response);
+    },
+    error: function (error) {
+      console.log("updateUserInfo error");
+      console.log(error);
+      console.log(error.responseText);
+    },
+  });
+});
 
 (function () {
   "use strict";
@@ -92,20 +81,17 @@ parentForm.addEventListener("submit",(event) => {
     });
 })();
 
+function undoEdit() {
+  $(".button--trigger").show();
+  $(this).removeClass("active").addClass("out");
 
-function undoEdit()  {
-  $('.button--trigger').show()
-    $(this).removeClass('active').addClass('out');
-
-        setTimeout(function(){
-          $('.button--disappear').hide()
-        }, 0); //Same time as animation
+  setTimeout(function () {
+    $(".button--disappear").hide();
+  }, 0); //Same time as animation
 }
 
-$('.button--trigger').on('click', function(e) {
-  $('.button--trigger').hide()
-  $('.button--disappear').show();
-  $('.button--disappear').removeClass('out').addClass('active');
+$(".button--trigger").on("click", function (e) {
+  $(".button--trigger").hide();
+  $(".button--disappear").show();
+  $(".button--disappear").removeClass("out").addClass("active");
 });
-});
-
