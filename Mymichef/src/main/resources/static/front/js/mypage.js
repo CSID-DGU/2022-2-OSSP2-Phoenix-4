16f18d46a10716b1a990e2e60696dd695147ad2f
DESKTOP-BJ8G1LU\nikph <2020113392@dgu.ac.kr>
2022-12-16 05:58:59 +0900

    크하하하

--- Mymichef/src/main/resources/static/front/js/mypage.js
+++ Mymichef/src/main/resources/static/front/js/mypage.js
@@ -164,3 +164,74 @@ $("#chat-label").click(function() {
   $('.live-chat-down-arrow').toggleClass('show-down-arrow');
 
 });
+
+
+var expression = "";
+var reset = false;
+
+function updateDisplay(val) {
+	// console.log(val);
+  expression = expression + val;
+  document.getElementById("display").innerHTML = expression;
+}
+
+function updateDisplayNumber(val) {
+	// console.log(val);
+  if (reset)
+    expression = val;
+  else
+    expression = expression + val;
+  document.getElementById("display").innerHTML = expression;
+}
+
+function computeExpression () {
+	// console.log(expression)
+	var result = eval(expression);
+	document.getElementById("display").innerHTML = result;
+	expression = result;
+}
+
+function clearDisplay() {
+	expression = "";
+  reset = true;
+	// console.log(expression);
+	document.getElementById("display").innerHTML = "0";
+}
+
+function deleteChar() {
+	if (expression.length > 1) {
+		expression = expression.substr(0, expression.length-1)
+		document.getElementById("display").innerHTML = expression;
+	} else {
+		document.getElementById("display").innerHTML = "0";
+	}
+}
+
+function computePercent() {
+}
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
